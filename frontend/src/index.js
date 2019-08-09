a3a7f3d5f3ef87761f4cdb18ec8cb7f637877440
Javier Granados <jgranadosflores@gmail.com>
2019-08-10 01:48:49 +0200

    Move assets folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../assets/css/normalize.css'
+import './assets/css/normalize.css'
 import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
