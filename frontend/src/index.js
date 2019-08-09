2479673f6fa49d2c4fe3240d4b033487e41b4953
Javier Granados <jgranadosflores@gmail.com>
2019-08-10 01:41:18 +0200

    Add normalize.css file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../assets/css/normalize.css'
 import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
