b42bd1affd7966b9ede8df764699a17ecd46bf63
Javier Granados <jgranadosflores@gmail.com>
2019-08-10 17:47:42 +0200

    Add eslint and prettier configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/css/styles.css'
-import './assets/css/normalize.css'
-import App from './components/App.jsx';
+/* eslint-env browser */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/styles.css";
+import "./assets/css/normalize.css";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
