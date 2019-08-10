88a0f6a034364ac3af91ba4ee91e9db02d3b472c
Javier Granados <jgranadosflores@gmail.com>
2019-08-10 15:55:48 +0200

    Create login page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import TextField from '@material-ui/core/TextField'
 
-const app = () => {
-  const text = "Hello World! 🚀"
-  return (
-    <div>
-      {text}
-    </div>
-  )
+class App extends Component {
+  state = {
+    error: false
+  }
+
+  render () {
+    return (
+      <div className="login-page">
+        <h1>Login page</h1>
+        <TextField
+          required
+          autoFocus
+          label="User"
+          variant="outlined"
+        />
+        <TextField
+          required
+          error={this.state.error}
+          type="password"
+          label="Password"
+          variant="outlined"
+        />
+      </div>
+    )
+  }
 }
 
-export default app
+export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/css/styles.css'
 import './assets/css/normalize.css'
 import App from './components/App.jsx';
 
